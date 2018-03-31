370d028b00e8c35255b27d1e65b275d472ef74bc
tkygtr6 <tkygtr6@gmail.com>
2018-03-31 18:09:30 +0900

    スレッドを実装した (Step8 終了)

--- 01/os/main.c
+++ 01/os/main.c
@@ -1,46 +1,24 @@
 #include "defines.h"
-#include "intr.h"
+#include "kozos.h"
 #include "interrupt.h"
-#include "serial.h"
 #include "lib.h"
 
-static void intr(softvec_type_t type, unsigned long sp){
-    int c;
-    static char buf[32]; /* 受信バッファの定義 */
-    static int len;
 
-    c = getc();
-
-    if(c != '\n'){
-        buf[len++] = c;
-    }else{
-        buf[len++] = '\0';
-        if(!strncmp(buf, "echo", 4)){
-            puts(buf + 4);
-            puts("\n");
-        }else{
-            puts("unknown.\n");
-        }
-        puts("> ");
-        len = 0;
-    }
+/* システムタスクとユーザスレッドの起動 */
+static int start_threads(int argc, char *argv[]){
+    kz_run(test08_1_main, "command", 0x100, 0, NULL);
+    return 0;
 }
 
-
 int main(void){
-    INTR_DISABLE;
+    INTR_DISABLE; /* 割り込み無効にする */
+
     puts("kozos boot succeed!\n");
 
-    softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
-    serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
+    /* OSの動作開始 */
+    kz_start(start_threads, "start", 0x100, 0, NULL);
 
-    puts("> ");
-
-    INTR_ENABLE;
-
-    while (1){
-        asm volatile ("sleep");
-    }
+    /* ここには戻ってこない */
 
     return 0;
 }
